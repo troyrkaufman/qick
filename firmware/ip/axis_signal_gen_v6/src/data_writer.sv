ac75a76cd20670c6b46d3ccfbc5d4f854a984afa
troyrkaufman <tkaufman@g.hmc.edu>
2025-10-05 12:35:14 -0700

    Finished writing data_writer.sv translation

--- firmware/ip/axis_signal_gen_v6/src/data_writer.sv
+++ firmware/ip/axis_signal_gen_v6/src/data_writer.sv
@@ -58,14 +58,109 @@ module data_writer #(
     logic [N-1:0] mem_addr_high_r;
 
     // Enable logic generation
-    genvar I;
+    genvar i;
     generate 
-        for(i = 0; i < NT-1; i++) begin 
-            mem_en_i[I] = (mem_addr_low == (NT-1:0)'d$unsigned(I)) ? 1 : 0;
+        for(i = 0; i < NT; i++) begin 
+            assign mem_en_i[i] = (mem_addr_low == i);
         end
     endgenerate
 
+    always_ff@(posedge clk) begin 
+        if (~rstn) begin
+            // Axis registers
+            tready_r <= 0;
+            tdata_r <= 0;
+            tdata_rr <= 0;
+            tdata_rrr <= 0;
+            tvalid_r <= 0;
+            tvalid_rr <= 0;
+            tvalid_rrr <= 0;
 
+            // Memory address
+            mem_addr_full <= 0;
+            mem_addr_high_r <= 0;
+            mem_en_r <= 0;
+        end else begin 
+            // Axis registers
+            tready_r <= tread_i;
+            tdata_r <= s_axis_tdata;
+            tvalid_r <= s_axis_tvalid;
 
+            // Extra registers to account pipe of state machine
+            tdata_rr <= tdata_r;
+            tdata_rrr <= tdata_rr;
+            tvalid_rr <= tvalid_r;
+            tvalid_rrr <= tvalid_rr;
+
+            // Memory address
+            if (READ_START_ADDR_ST == 1) begin 
+                mem_addr_full <= $unsigned(int(START_ADDR_REG));
+            end else if (RW_TDATA_ST == 1) begin 
+                mem_addr_full <= mem_addr_full + 1;
+            end
+
+            mem_addr_high_r <= mem_addr_high;
+            mem_en_r <= mem_en_i;
+        end
+    end
+
+    // Address computation
+    mem_addr_low <= mem_addr_full[NT_LOG2-1:0];
+    mem_addr_high <= mem_addr_full[NT_LOG2+N-1:NT_LOG2];
+
+    // Finite State Machine
+    always_ff@(posedge clk) begin 
+        if (~rstn) 
+            state <= INIT_ST;
+        else 
+            case (state)
+                INIT_ST: 
+                    if (WE_REG_resync) state <= READ_START_ADDR_ST;
+                READ_START_ADDR_ST:    state <= WAIT_TVALID_ST;
+                WAIT_TVALID_ST: 
+                    if (WE_REG_resync) begin 
+                        if (~tvalid_r) 
+                            state <= WAIT_TVALID_ST;
+                        else 
+                            state <= RW_TDATA_ST;
+                    end else begin
+                        state <= INIT_ST; 
+                    end
+                RW_TDATA_ST: 
+                    if (~tvalid_r)
+                        state <= WAIT_TVALID_ST;
+            endcase
+    end
+
+    logic read_start_addr_state;
+    logic rw_tdata_state;
+    logic tready_i;
+
+    // Output logic
+    always_comb begin 
+        read_start_addr_state = 0;
+        rw_tdata_state = 0;
+        tready_i = 0;
+
+        case (state)
+            INIT_ST: 
+            READ_START_ADDR_ST: 
+                read_start_addr_state = 1;
+            WAIT_TVALID_ST: 
+                tready_i = 1;
+            RW_TDATA_ST: begin 
+                rw_tdata_state = 1;
+                tread_i = 1;
+            end
+        endcase
+    end
+
+    // Assign output
+    assign s_axis_tready = tready_r;
+
+    assign mem_en = mem_en_r;
+    assign mem_we = tvalid_rrr;
+    assign mem_addr = mem_addr_high_r;
+    assign mem_di = tdata_rrr;
 
 endmodule
